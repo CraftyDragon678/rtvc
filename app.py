32d4837bbbe6602fb45578e70f96932252531c97
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-04 23:32:38 +0900

    feat: support https

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import dotenv
 import api
+import os, ssl
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
 
     app = api.AgayaApi()
-    app.run('0.0.0.0', debug=True)
+    debug = bool(os.getenv("DEBUG"))
+
+    if os.getenv("HTTPS") == "true":
+        ssl_contxt = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        ssl_contxt.load_cert_chain(certfile=os.getenv("CERT"), keyfile=os.getenv("KEY"))
+
+        app.run('0.0.0.0', debug=debug, ssl_context=ssl_contxt)
+    else:
+        app.run('0.0.0.0', debug=debug)
 
