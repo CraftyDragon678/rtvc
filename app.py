2c434409a3eea1712605b6e0ba8f90255be86c57
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-04 19:45:30 +0900

    feat: connect to db

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import dotenv
 import api
+import db
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
+
+    db.AgayaDBClient()
     app = api.AgayaApi()
     app.run('0.0.0.0', debug=True)
 
