da07dde8b75798d96245b083696d34a1cfc757a9
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-05 08:55:14 +0900

    feat: fetch user information from resource server

--- api/auth.py
+++ api/auth.py
@@ -1,6 +1,9 @@
+from pymongo.database import Database
 from flask_restplus import Namespace, Resource, fields, reqparse
 import requests
-import os
+import os, datetime
+
+datetime.datetime.strftime
 
 api = Namespace('auth')
 
@@ -21,13 +24,31 @@ class Kakao(Resource):
             'code': args['code'],
             'client_secret': os.getenv("KAKAO_CLIENT_SECRET")
         })
-        if res.status_code == 200:
-            data = res.json()
-            return {
-                'token_type': data['token_type'],
-                'access_token': data['access_token'],
-                'refresh_token': data['refresh_token'],
-                'expires_in': data['expires_in'],
-                'refresh_token_expires_in': data['refresh_token_expires_in']
-            }
-        return {'status': 'fail'}, 400
+        if res.status_code != 200:
+            return {'status': 'fail'}, 400
+
+        data = res.json()
+        db: Database = self.api.db
+
+        res = requests.get("https://kapi.kakao.com/v2/user/me", headers={
+            'Authorization': "Bearer " + data['access_token']
+        })
+        if res.status_code != 200:
+            return {'status': 'fail'}, 400
+
+        me = res.json()
+        userdata = {
+            'type': "KAKAO",
+            'connected_at': datetime.datetime.strptime(me['connected_at'], "%Y-%m-%dT%H:%M:%SZ"),
+            'nickname': me['kakao_account']['profile']['nickname']
+        }
+        
+        db['users'].update({'_id': me['id']}, userdata, upsert=True)
+
+        return {
+            'token_type': data['token_type'],
+            'access_token': data['access_token'],
+            'refresh_token': data['refresh_token'],
+            'expires_in': data['expires_in'],
+            'refresh_token_expires_in': data['refresh_token_expires_in']
+        }
