cd2e6c01cbc250a7a4321f04812934b1e95ec9e3
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-07 20:32:05 +0900

    test: auth_required

--- api/test.py
+++ api/test.py
@@ -1,8 +1,10 @@
 from flask_restplus import Namespace, Resource, fields
+import utils
 
 api = Namespace('test', description='test namespace')
 
 @api.route("/")
 class Test(Resource):
+    @utils.auth_required
     def get(self):
         return "test"
