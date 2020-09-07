e52ed185cae4becb8dd2d7b632c175caf00c0279
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-07 20:05:22 +0900

    feat: add auth required decorator

---
+++ utils/auth.py
@@ -0,0 +1,23 @@
+from flask import request, current_app, g
+from functools import wraps
+import jwt
+
+
+def auth_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get("Authorization")
+        if token is None:
+            return "", 401
+
+        try:
+            payload = jwt.decode(token, current_app.config['JWT_SECRET_KEY'], "HS256")
+        except jwt.InvalidTokenError:
+            payload = None
+        
+        if payload is None:
+            return "", 401
+        
+        g.user = payload
+        return func(*args, **kwargs)
+    return wrapper
