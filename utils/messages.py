1b918a2d85c12886491586f79e69da3f11f31c86
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-08 18:31:10 +0900

    feat: add invalid objectid message

--- utils/messages.py
+++ utils/messages.py
@@ -2,5 +2,6 @@ ERROR_MESSAGES = {
     'no_token': "no token provided",
     'not_valid_token': "token is not valid",
     'user_not_exist': "specific user doesn't exist",
-    'no_user': "please specific the user who %s"
+    'no_user': "please specific the user who %s",
+    'invalid_objectid': "ObjectId must be a 12-byte input or a 24-character hex string",
 }
