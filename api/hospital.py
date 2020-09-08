10c71924dc32a47fc09d51ad4f93b424f4753b3d
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-08 21:51:12 +0900

    feat: hospital reqparse

--- api/hospital.py
+++ api/hospital.py
@@ -1,8 +1,16 @@
-from flask_restplus import Namespace, Resource
+from flask_restplus import Namespace, Resource, reqparse
 
 api = Namespace('hospital')
 
 @api.route("/list")
 class List(Resource):
+    @api.param('category', "OS 정형외과/DR 피부과/GS 일반외과/MG 내과", enum=['OS', 'DR', 'GS', 'MG'])
+    @api.param('lat', "위도", type=float)
+    @api.param('lng', "경도", type=float)
     def get(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('category')
+        parser.add_argument('lat', type=float)
+        parser.add_argument('lng', type=float)
+        args = parser.parse_args()
+        return {'category': args['category'], 'lat': args['lat'], 'lng': args['lng']}
