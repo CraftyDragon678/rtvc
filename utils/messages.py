d2833328d3492dad42985c56ee05429476eb5bd7
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-08 17:43:59 +0900

    feat: add error messages

--- utils/messages.py
+++ utils/messages.py
@@ -1,4 +1,6 @@
 ERROR_MESSAGES = {
     'no_token': "no token provided",
-    'not_valid_token': "token is not valid"
+    'not_valid_token': "token is not valid",
+    'user_not_exist': "specific user doesn't exist",
+    'no_user': "please specific the user who %s"
 }
