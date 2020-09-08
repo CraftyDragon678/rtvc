e3678e5a80db2b9fd1abd051a457f79fcc804000
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-08 18:33:05 +0900

    feat: add not_exist error message

--- utils/messages.py
+++ utils/messages.py
@@ -4,4 +4,5 @@ ERROR_MESSAGES = {
     'user_not_exist': "specific user doesn't exist",
     'no_user': "please specific the user who %s",
     'invalid_objectid': "ObjectId must be a 12-byte input or a 24-character hex string",
+    'not_exist': "data doesn't exist"
 }
