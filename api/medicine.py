e41bd03837543e55d31229cd6c1829f7780b0974
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-13 10:38:59 +0900

    feat: get now medicine

--- api/medicine.py
+++ api/medicine.py
@@ -3,25 +3,9 @@ from flask_restplus import Namespace, Resource, fields
 from pymongo.database import Database
 import utils
 from datetime import datetime
+import dateutil
 
 api = Namespace('medicine')
-
-MEDICINES = [
-    {
-        "start": datetime(2019, 8, 17),
-        "end": datetime(2019, 8, 19),
-        "time": "morning",
-        "name": "약약",
-        "amount": 1
-    },
-    {
-        "start": datetime(2020, 9, 7),
-        "end": datetime(2020, 9, 19),
-        "time": "morning",
-        "name": "약약",
-        "amount": 1
-    },
-]
 
 @api.route("/<date>")
 @api.param("date", "2006-01-02 같은 형태로")
@@ -38,9 +22,10 @@ class Medicine(Resource):
     def post(self, date):
         data = request.json
         db: Database = self.api.db
-        # db['medicine'].insert_one({
-
-        # })
+        data['from']
+        db['medicine'].insert_one({
+            
+        })
 
 
 @api.route("/now")
@@ -48,7 +33,11 @@ class Now(Resource):
     @api.doc(security="jwt")
     @utils.auth_required
     def get(self):
-        date = (datetime.datetime.utcnow() + datetime.timedelta(hours=9)).strftime("%Y-%m-%d")
-        print(date)
-        medicines = list(filter(lambda x: x['date'] == date, MEDICINES))
-        return medicines
+        db: Database = self.api.db
+        today = datetime.now()
+        res = db['medicine'].find({
+            'start': {'$lte': today},
+            'end': {'$gte': today},
+            'who': g.user['_id']
+        }, projection={'_id': 0, 'time': 1, 'name': 1, 'amount': 1})
+        return {'medicines': list(res)}
