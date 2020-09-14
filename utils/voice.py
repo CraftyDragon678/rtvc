cd233589ac3603cb65d4fe85f0df5778851f587f
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-14 20:14:41 +0900

    fix: wrap Path class in load weight

--- utils/voice.py
+++ utils/voice.py
@@ -1,6 +1,7 @@
 import librosa
 import soundfile as sf
 import numpy as np
+from pathlib import Path
 
 from synthesizer.inference import Synthesizer
 from vocoder import inference as vocoder
@@ -8,9 +9,9 @@ from encoder import inference as encoder
 
 class TTS():
     def __init__(self):
-        encoder.load_model("encoder/saved_models/pretrained.pt")
-        self.synthesizer = Synthesizer("synthesizer/saved_models/logs-pretrained/taco_pretrained")
-        vocoder.load_model("vocoder/saved_models/pretrained/pretrained.pt")
+        encoder.load_model(Path("encoder/saved_models/pretrained.pt"))
+        self.synthesizer = Synthesizer(Path("synthesizer/saved_models/logs-pretrained/taco_pretrained"))
+        vocoder.load_model(Path("vocoder/saved_models/pretrained/pretrained.pt"))
 
     def synthesize(self, f):
         preprocessed_wav = encoder.preprocess_wav(f)
