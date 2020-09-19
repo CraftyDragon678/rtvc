60fdadcf3281deed9683e26919907e058c8c7183
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-19 13:19:37 +0900

    feat: port option

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
 
     app = api.AgayaApi()
     debug = bool(os.getenv("DEBUG"))
+    port = os.getenv("PORT") or 5000
 
     if os.getenv("HTTPS") == "true":
         ssl_contxt = ssl.SSLContext(ssl.PROTOCOL_TLS)
         ssl_contxt.load_cert_chain(certfile=os.getenv("CERT"), keyfile=os.getenv("KEY"))
 
-        app.run('0.0.0.0', debug=debug, ssl_context=ssl_contxt)
+        app.run('0.0.0.0', port=port, debug=debug, ssl_context=ssl_contxt)
     else:
-        app.run('0.0.0.0', debug=debug)
+        app.run('0.0.0.0', port=port, debug=debug)
 
