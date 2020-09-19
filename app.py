666cfc50c13acd871f51fb6d24f33c54bb24ea1c
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-19 13:35:55 +0900

    feat: support wsgi

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import dotenv
 import api
 import os, ssl
 
-if __name__ == "__main__":
+def create_app():
     dotenv.load_dotenv()
-
     app = api.AgayaApi()
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
     debug = bool(os.getenv("DEBUG"))
     port = os.getenv("PORT") or 5000
 
@@ -13,7 +16,7 @@ if __name__ == "__main__":
         ssl_contxt = ssl.SSLContext(ssl.PROTOCOL_TLS)
         ssl_contxt.load_cert_chain(certfile=os.getenv("CERT"), keyfile=os.getenv("KEY"))
 
-        app.run('0.0.0.0', port=port, debug=debug, ssl_context=ssl_contxt)
+        app.run(port=port, debug=debug, ssl_context=ssl_contxt)
     else:
-        app.run('0.0.0.0', port=port, debug=debug)
+        app.run(port=port, debug=debug)
 
