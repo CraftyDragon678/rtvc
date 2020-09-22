748885247eafc829bf95af6f8ca65f194a4b5bfa
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-22 21:49:14 +0900

    feat: nugu_auth decorator

--- utils/auth.py
+++ utils/auth.py
@@ -22,3 +22,22 @@ def auth_required(func):
         g.user = payload
         return func(*args, **kwargs)
     return wrapper
+
+def nugu_auth_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.args.get('token')
+        if token is None:
+            return {"message": utils.ERROR_MESSAGES['no_token']}, 401
+
+        try:
+            payload = jwt.decode(token, current_app.config['JWT_SECRET_KEY'], "HS256")
+        except jwt.InvalidTokenError:
+            payload = None
+        
+        if payload is None:
+            return {"message": utils.ERROR_MESSAGES['invalid_token']}, 401
+        
+        g.user = payload
+        return func(*args, **kwargs)
+    return wrapper
