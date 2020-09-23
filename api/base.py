ea2cc8e463a1d6c0b086c2cc6dcf8b1b1af1f663
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-23 22:09:24 +0900

    chore: remove unused static option

--- api/base.py
+++ api/base.py
@@ -16,7 +16,7 @@ import os
 
 class AgayaApi(Flask):
     def __init__(self):
-        super().__init__(__name__, static_folder='files')
+        super().__init__(__name__)
         self.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
         self.api = Api(self, title="Agaya API", version='0.2', authorizations={
